315f6f2f4305f37ee113b31bca5da15e7161a0d5
meriam <hamdaoui.meriam85@gmail.com>
2022-03-19 09:35:15 +0100

    morning update

--- ES6.js
+++ ES6.js
@@ -138,7 +138,7 @@ function XO(str) {
 
 /*square every digit
 https://www.codewars.com/kata/546e2562b03326a88e000020*/
-function squareDigits(num){ //my solution cleaver me ehehe
+function squareDigits(num){ //my solution clever me ehehe
   return parseFloat(num.toString().split('').map(nbr => nbr*nbr).join(''));
 }
 
@@ -247,7 +247,7 @@ function isIsogram(str){
 
 /*list filtering
 https://www.codewars.com/kata/53dbd5315a3c69eed20002dd*/
-//my solution cleaver me
+//my solution clever me
 function filter_list(l) {
   return l.filter(function(v) {return typeof v == 'number'})
 }
@@ -425,11 +425,282 @@ function maskify(cc) {
 
  /**find the divisors
   * https://www.codewars.com/kata/544aed4c4a30184e960010f4 */
- function divisors(n){
-   while(n/i !== 1){
-     i++;
-   }
- }
+function divisors(integer) {
+  var divs = [];
+  
+  for(var i = 2; i < integer; i++) {
+    if(integer % i === 0) {
+      divs.push(i);
+    }
+  }
+  
+  return divs.length ? divs : integer + ' is prime';
+};
+//
+function divisors(integer){
+  var res = []
+  for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
+  return res.length ? res : integer + ' is prime';
+}
+
+
+
+
+/*************************Data Types*************************** */
+/**return negatives
+ * https://www.codewars.com/kata/55685cd7ad70877c23000102 */
+ //my solution
+ function makeNegative(num) {
+  // Code?
+  if (num > 0){
+    return -Math.abs(num);
+  }else{
+    return num;
+  }
+}
+//optimal solution from codewars
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
+
+
+/**remove first and last character
+ * https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0*/
+ //clever girl <3
+ function removeChar(str){
+  //You got this!
+   return str.substr(1,str.length-2); 
+ };
+ //codewars solution
+ function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+
+/**remove string space
+ * https://www.codewars.com/kata/57eae20f5500ad98e50002c5 */
+function noSpace(x){
+  return x.split(' ').join('');
+  //or return x.replace(/\s/g, '');
+}
+
+/**convert boolean value to string
+ * https://www.codewars.com/kata/53369039d7ab3ac506000467 */
+ function boolToWord( bool ){
+  //...
+  return bool === true ? 'Yes' : 'No';
+}
+
+/**convert a number to string
+ * https://www.codewars.com/kata/5265326f5fda8eb1160004c8 */
+ //clever me :D
+ function numberToString(num) {
+ return num.toString();
+ //or return String(num)
+} 
+
+
+
+/************************ Numbers **********************/
+/**Basic mathematical operations
+ * https://www.codewars.com/kata/57356c55867b9b7a60000bd7 */
+//my solution
+function basicOp(op, a, b)
+{
+  // Code
+  switch (op)
+  { case "+": return a + b
+    case "-": return a - b
+    case "*": return a * b
+    case "/": return a / b
+    case "%": return a % b
+    case "^": return Math.pow(a, b)
+    default: return "error"
+  }
+}
+//codewars
+function basicOp(op, a, b) {
+  return eval(a+op+b);
+}
+
+
+/**sum of the 1st nth term of series
+ * https://www.codewars.com/kata/555eded1ad94b00403000071 */
+function SeriesSum(n) {
+  var sum = 0;
+  for(var i = 0; i < n; i++) {
+    sum += 1 / (3 * i + 1);
+  }
+  return sum.toFixed(2);
+}
+//optimal
+function SeriesSum(n, s = 0) {
+  return n ? SeriesSum(n - 1, s + 1 / (3 * n - 2)) : s.toFixed(2)
+}
+
+
+/**keep hydrated
+ * https://www.codewars.com/kata/582cb0224e56e068d800003c */
+function litres(time) {
+  return Math.floor(time * 0.5);
+}//floor rounds a number to the nearst down integer
+
+
+/**century from year
+ * https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097 */
+function century(year) {
+  // not a perfect solution 
+  return Number(String(year).split('').slice(0,2).join(''))+ 1;
+}
+//codewars
+function century(year) {
+  return (year + 99) / 100 | 0;
+}
+
+
+/**beginner lost without a map
+ * https://www.codewars.com/kata/57f781872e3d8ca2a000007e */
+function maps(x){
+  return x.map(x => x * 2);
+}
+
+
+
+/*********************String****************** */
+/**reverse a string
+ *https://www.codewars.com/kata/5168bb5dfe9a00b126000018*/
+ //clever me :p
+function solution(str){
+  return str.split('').reverse().join('');
+}
+
+
+/**string endswith ?
+ * https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d */
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+/**do i get a bonus
+ * https://www.codewars.com/kata/56f6ad906b88de513f000d96 */
+//good job maya
+function bonusTime(salary, bonus) {
+  // your code here
+    return bonus === true ? "£"+String(salary*10) : "£"+String(salary);
+}
+
+
+/**abbreviate two word name
+ * https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3*/
+//my solution
+function abbrevName(name){
+  var arr = name.split(" ");
+  return arr[0].slice(0,1).toUpperCase()+"."+arr[1].slice(0,1).toUpperCase();
+}
+//optimal
+function abbrevName(name){
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+
+/**DNA to RNA conversion
+ * https://www.codewars.com/kata/5556282156230d0e5e000089 */
+function DNAtoRNA(dna){
+  return dna.replace(/T/g, 'U');
+}
+
+
+
+
+
+/*****************************Arrays****************************/
+/**counting sheep
+ * https://www.codewars.com/kata/54edbc7200b811e956000556 */
+//good job maya <3
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  return arrayOfSheep.filter(sheep => sheep === true).length;
+  //or  return arrayOfSheeps.filter(Boolean).length;
+}
+
+/** Ones and Zeros
+ * https://www.codewars.com/kata/578553c3a1b8d5c40300037c */
+ //clever maya kiss
+const binaryArrayToNumber = arr => {
+  // your code
+  return parseInt(arr.join(''),2);
+};
+
+
+/** a needle in the haystack
+ * https://www.codewars.com/kata/56676e8fabd2d1ff3000000c*/
+//my solution ha ha
+function findNeedle(haystack) {
+  // your code here
+  if (haystack.findIndex( needle => needle === "needle") >= 0 ){
+    return `found the needle at position ${haystack.findIndex(needle => needle === "needle")}`;
+  } else{
+    return "needle not found";
+  }   
+}
+//optimal
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+
+
+/**remove the minimum
+ * https://www.codewars.com/kata/563cf89eb4747c5fb100001b */
+ function removeSmallest(numbers) {
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
+//an other one
+function removeSmallest(numbers) {
+  const min = Math.min.apply(this, numbers);
+  return numbers.filter((num, idx, arr) => idx !== arr.indexOf(min));
+}
+
+
+/**convert number o reversed array of digital
+ *https://www.codewars.com/kata/5583090cbe83f4fd8c000051 */
+//saèiti mayoucha muwaaah
+function digitize(n) {
+  return String(n).split("").map((n)=>{
+    return Number(n)
+  }).reverse();
+}
+
+
+
+
+
+/********************objects*********************************/
+/**Make a function that does arithmetic
+ * https://www.codewars.com/kata/583f158ea20cfcbeb400000a */
+//my solution
+ function arithmetic(a, b, operator){
+  switch (operator)
+    { case "add": return a + b
+      case "subtract": return a - b
+      case "multiply": return a * b
+      case "divide": return a / b    
+      default:
+    }
+}
+//codewars
+function arithmetic(a, b, operator){
+  optable = { "add":"+", "subtract": "-", "multiply": "*" , "divide":"/"};
+  return eval(a + optable[operator] + b); 
+}
+
+
+/**Regular ball super ball
+ * https://www.codewars.com/kata/53f0f358b9cb376eca001079 */
+
+
+
 
 
 
